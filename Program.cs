183119547ae23fece51af055d11bc8d2dd48041d
Moazz <moazzemh222@gmail.com>
2022-12-18 00:36:55 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class
+{
+    class Animal
+    {
+        string name = "Audi";
+        int age = 6;
+        
+        public void printDetails()
+        {
+            Console.WriteLine("Name " + name + " " + "Age " + age);
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Animal();
+            dog.printDetails();
+        }
+    }
+}
